9d183277f1b8324158e76f158cc2db322a4bde9c
Andrews1112005 <andrewbppn@gmail.com>
2026-04-28 19:01:12 +0800

    add code for Web

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function blog(){
+    return (
+    <div>
+        <p>Ini adalah halaman blog</p>
+        <input  type = "text" placeholder="Masukkan teks disini "  /> 
+        <button> click me </button>
+    </div>
+    );
+}
---
+++ app/learn/page.tsx
@@ -0,0 +1,14 @@
+export default function LearnPage(){
+    return (
+    <div>
+        <div>
+            <img src="dr.jpg" alt="" />
+            <p>logo desain</p>
+        </div>
+        <div>
+            <h1>Figma</h1>
+            <p>8 hours Ago</p>
+        </div>
+    </div>
+    );
+}
